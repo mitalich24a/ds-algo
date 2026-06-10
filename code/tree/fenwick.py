7c87f5ad6db198a3320e5dc68f4962775cd6d251
mitalich24a <mitalic209@gmail.com>
2026-06-10 09:02:43 +0530

    Update fenwick.py

--- code/tree/fenwick.py
+++ code/tree/fenwick.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 class BaseFenwickTree(ABC):
     def __init__(self, size):
         self.size = size
@@ -5,57 +7,62 @@ class BaseFenwickTree(ABC):
 
     @property
     @abstractmethod
-    def neutral_value(self): pass
+    def neutral_value(self): 
+        pass
 
     @abstractmethod
-    def _merge(self, current, delta): pass
+    def _combine(self, current, delta): 
+        """Combines the current tree value with an incoming update."""
+        pass
 
     @abstractmethod
-    def _inverse(self, right_prefix, left_prefix): pass
+    def _difference(self, right_prefix, left_prefix): 
+        """Calculates the difference to isolate the range query."""
+        pass
 
     def update(self, idx, value):
         while idx <= self.size:
-            self.tree[idx] = self._merge(self.tree[idx], value)
+            self.tree[idx] = self._combine(self.tree[idx], value)
             idx = idx + (idx & (-idx))
 
     def query(self, idx):
         res = self.neutral_value
         while idx > 0:
-            res = self._merge(res, self.tree[idx])
+            res = self._combine(res, self.tree[idx])
             idx = idx - (idx & (-idx))
         return res
 
     def range_query(self, left, right):
-        return self._inverse(self.query(right), self.query(left - 1))
+        return self._difference(self.query(right), self.query(left - 1))
 
 
 class SumFenwick(BaseFenwickTree):
     neutral_value = 0
 
-    def _merge(self, current, delta):
+    def _combine(self, current, delta):
         return current + delta
 
-    def _inverse(self, r, l):
-        return r - l
+    def _difference(self, right_prefix, left_prefix):
+        return right_prefix - left_prefix
 
 
 class XorFenwick(BaseFenwickTree):
     neutral_value = 0
 
-    def _merge(self, current, delta):
+    def _combine(self, current, delta):
         return current ^ delta
 
-    def _inverse(self, r, l):
-        # XOR is its own inverse, so we use ^ instead of -
-        return r ^ l
+    def _difference(self, right_prefix, left_prefix):
+        # XORing a prefix cancels it out, serving as the "difference"
+        return right_prefix ^ left_prefix
 
 
 class ProdFenwick(BaseFenwickTree):
     neutral_value = 1
 
-    def _merge(self, current, delta):
+    def _combine(self, current, delta):
         return current * delta
 
-    def _inverse(self, r, l):
-        # Using integer division to undo the prefix multiplications
-        return r // l
+    def _difference(self, right_prefix, left_prefix):
+        # Using integer division to find the difference between product prefixes
+        return right_prefix // left_prefix
